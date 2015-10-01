3b8b647738df720b7be4ebb9fe1a5ff77a73f98d
Peter Gerhat <swyna@swynasharing-2.local>
2015-10-01 09:41:05 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,31 @@
+## -*- Makefile -*-
+##
+## User: swyna
+## Time: May 21, 2012 1:37:16 AM
+## Makefile created by Sun Studio.
+##
+## This file is generated automatically.
+##
+
+
+all: spheres1 spheres2 spheres3
+
+## Target: spheres1
+spheres1: src/stopWatch/stopWatch.h
+
+
+## Target: spheres2
+spheres2: src/stopWatch/stopWatch.h
+
+
+## Target: spheres3
+spheres3: src/stopWatch/stopWatch.h
+
+
+#### Clean target deletes all generated files ####
+clean:
+
+# Enable dependency checking
+.KEEP_STATE:
+.KEEP_STATE_FILE:.make.state.GNU-x86_64-MacOSX
+
---
+++ nbproject/Makefile-Release.mk
@@ -0,0 +1,103 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-MacOSX
+CND_CONF=Release
+CND_DISTDIR=dist
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=build/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src/main.o \
+	${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src/init.o \
+	${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src/utils.o \
+	${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src/print.o \
+	${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src/stopWatch/stopWatch.o
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	${MAKE}  -f nbproject/Makefile-Release.mk dist/Release/GNU-MacOSX/cudatest
+
+dist/Release/GNU-MacOSX/cudatest: ${OBJECTFILES}
+	${MKDIR} -p dist/Release/GNU-MacOSX
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cudatest ${OBJECTFILES} ${LDLIBSOPTIONS} 
+
+${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src/main.o: nbproject/Makefile-${CND_CONF}.mk ../CUDA/HW06/src/main.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src/main.o ../CUDA/HW06/src/main.cpp
+
+${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src/init.o: nbproject/Makefile-${CND_CONF}.mk ../CUDA/HW06/src/init.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src/init.o ../CUDA/HW06/src/init.cpp
+
+${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src/utils.o: nbproject/Makefile-${CND_CONF}.mk ../CUDA/HW06/src/utils.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src/utils.o ../CUDA/HW06/src/utils.cpp
+
+${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src/print.o: nbproject/Makefile-${CND_CONF}.mk ../CUDA/HW06/src/print.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src/print.o ../CUDA/HW06/src/print.cpp
+
+${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src/stopWatch/stopWatch.o: nbproject/Makefile-${CND_CONF}.mk ../CUDA/HW06/src/stopWatch/stopWatch.cpp 
+	${MKDIR} -p ${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src/stopWatch
+	${RM} $@.d
+	$(COMPILE.cc) -O2 -MMD -MP -MF $@.d -o ${OBJECTDIR}/_ext/_DOTDOT/CUDA/HW06/src/stopWatch/stopWatch.o ../CUDA/HW06/src/stopWatch/stopWatch.cpp
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf:
+	${RM} -r build/Release
+	${RM} dist/Release/GNU-MacOSX/cudatest
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
